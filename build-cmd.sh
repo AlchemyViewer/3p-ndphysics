0378f493dbb696f2e99bbbac37b11450c9ca9a66
Rye Mutt <rye@alchemyviewer.org>
2020-10-12 05:49:53 -0400

    Fix linux build

--- build-cmd.sh
+++ build-cmd.sh
@@ -87,25 +87,75 @@ case "$AUTOBUILD_PLATFORM" in
 	    cp "$stage/Source/Pathing/libnd_Pathing.a" "$stage/lib/release/"
 	    cp "$stage/Source/HACD_Lib/libhacd.a" "$stage/lib/release/"
 	;;
-	"linux")
-	    cmake ../
-	    CFLAGS="-m32 -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC -DPIC -O2 -g" \
-                CXXFLAGS="-m32 -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC -DPIC -O2 -g -std=c++11" \
-                make
+	linux*)
+            # Linux build environment at Linden comes pre-polluted with stuff that can
+            # seriously damage 3rd-party builds.  Environmental garbage you can expect
+            # includes:
+            #
+            #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
+            #    DISTCC_LOCATION            top            branch      CC
+            #    DISTCC_HOSTS               build_name     suffix      CXX
+            #    LSDISTCC_ARGS              repo           prefix      CFLAGS
+            #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
+            #
+            # So, clear out bits that shouldn't affect our configure-directed build
+            # but which do nonetheless.
+            #
+            unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+        
+            # Default target per --address-size
+            opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE}"
+            DEBUG_COMMON_FLAGS="$opts -Og -g -fPIC -DPIC"
+            RELEASE_COMMON_FLAGS="$opts -O3 -g -fPIC -fstack-protector-strong -DPIC -D_FORTIFY_SOURCE=2"
+            DEBUG_CFLAGS="$DEBUG_COMMON_FLAGS"
+            RELEASE_CFLAGS="$RELEASE_COMMON_FLAGS"
+            DEBUG_CXXFLAGS="$DEBUG_COMMON_FLAGS -std=c++17"
+            RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
+            DEBUG_CPPFLAGS="-DPIC"
+            RELEASE_CPPFLAGS="-DPIC -D_FORTIFY_SOURCE=2"
+        
+            JOBS=`cat /proc/cpuinfo | grep processor | wc -l`
 
-	    cp "$stage/Source/lib/libnd_hacdConvexDecomposition.a" "$stage/lib/release/"
-	    cp "$stage/Source/Pathing/libnd_Pathing.a" "$stage/lib/release/"
-	    cp "$stage/Source/HACD_Lib/libhacd.a" "$stage/lib/release/"
-	;;
-	"linux64")
-	    cmake ../
-	    CFLAGS="-m64 -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC -DPIC -O2 -g" \
-                CXXFLAGS="-m64 -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC -DPIC -O2 -g -std=c++11" \
-                make
+            # Handle any deliberate platform targeting
+            if [ -z "${TARGET_CPPFLAGS:-}" ]; then
+                # Remove sysroot contamination from build environment
+                unset CPPFLAGS
+            else
+                # Incorporate special pre-processing flags
+                export CPPFLAGS="$TARGET_CPPFLAGS"
+            fi
 
-	    cp "$stage/Source/lib/libnd_hacdConvexDecomposition.a" "$stage/lib/release/"
-	    cp "$stage/Source/Pathing/libnd_Pathing.a" "$stage/lib/release/"
-	    cp "$stage/Source/HACD_Lib/libhacd.a" "$stage/lib/release/"
+            # Debug
+            mkdir -p "build_debug"
+            pushd "build_debug"
+                CFLAGS="$DEBUG_CFLAGS" \
+                CXXFLAGS="$DEBUG_CXXFLAGS" \
+                CPPFLAGS="$DEBUG_CPPFLAGS" \
+                    cmake $srcdir -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$stage"
+
+                make -j$JOBS
+
+                mkdir -p ${stage}/lib/debug
+	            cp "Source/lib/libnd_hacdConvexDecomposition.a" "$stage/lib/debug/"
+	            cp "Source/Pathing/libnd_Pathing.a" "$stage/lib/debug/"
+	            cp "Source/HACD_Lib/libhacd.a" "$stage/lib/debug/"
+            popd
+
+            # Release
+            mkdir -p "build_release"
+            pushd "build_release"
+                CFLAGS="$RELEASE_CFLAGS" \
+                CXXFLAGS="$RELEASE_CXXFLAGS" \
+                CPPFLAGS="$RELEASE_CPPFLAGS" \
+                    cmake $srcdir -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$stage"
+
+                make -j$JOBS
+
+                mkdir -p ${stage}/lib/release
+	            cp "Source/lib/libnd_hacdConvexDecomposition.a" "$stage/lib/release/"
+	            cp "Source/Pathing/libnd_Pathing.a" "$stage/lib/release/"
+	            cp "Source/HACD_Lib/libhacd.a" "$stage/lib/release/"
+            popd
 	;;
 esac
 
